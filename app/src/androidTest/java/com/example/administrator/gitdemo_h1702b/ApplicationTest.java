25b405ace7ae87d898863444092b755d11b6ed55
liyapengyali@163.com <liyapengyali@163.com>
2017-09-12 09:43:13 +0800

    提交代码

---
+++ app/src/androidTest/java/com/example/administrator/gitdemo_h1702b/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.administrator.gitdemo_h1702b;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
